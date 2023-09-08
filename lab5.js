45466f60633270f8b14d30ce5795397f0a867983
Srijana Lama <srijana.lama@miu.edu>
2023-09-08 15:58:42 -0500

    changes

--- lab5.js
+++ lab5.js
@@ -1,9 +1,20 @@
 // Solution 1 :
+let array1 = [2,20,50,40,3,2] ;
+function sum(array1){
+  return array1.filter(x => x > 20).reduce((accum,current) => accum + current);
 
-let sum = [2,20,50,40,3,2].filter(x => x > 20).reduce((accum,current) => accum + current);
+}
+console.log(sum(array1));
 
 
 //Solution 2
-let results = ["apple", "banana", "cherry", "date", "grape"].filter(
-    word => word.length >= 5 && word.includes('a')
-);
+let results =  
+    ["apple", "banana", "cherry", "date", "grape"].filter(
+        function(str) {
+            return str.length >= 5 && str.includes('a');
+          }
+    );
+
+
+
+
